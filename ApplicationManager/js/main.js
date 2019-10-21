9c4e8486e17173a5ba556e9212996d9260972e01
Georgi Tsvetkov <tsvetkov_george@abv.bg>
2019-10-21 08:34:58 +0300

    fixed save submission array splice issue

--- ApplicationManager/js/main.js
+++ ApplicationManager/js/main.js
@@ -86,19 +86,19 @@ function createTableRow() {
 
 function editSubmission(rowNumber) {  
     var tableEdit = document.getElementById('displayInfo');
-    tableEdit.rows[rowNumber].cells[1].innerHTML = '<input id="firstName" type="text" name="firstName" placeholder="Enter your name here:" required>';
-    tableEdit.rows[rowNumber].cells[2].innerHTML = '<input id="surname" type="text" name="surname" placeholder="Enter your surname here:" required>';
-    tableEdit.rows[rowNumber].cells[3].innerHTML = '<input id="email" name="email" type="text" placeholder="Enter your email address here:" required>';
-    tableEdit.rows[rowNumber].cells[4].innerHTML = '<input id="age" type="number" name="age" min="18" max="99" placeholder="Your age:" required>';
-    tableEdit.rows[rowNumber].cells[5].innerHTML = '<input id="gsm" type="text" name="phoneNumber" placeholder="Enter your phone number here:" required>';
-    tableEdit.rows[rowNumber].cells[6].innerHTML = '<input id="choice" type="radio" name="communication" value="phone"><strong>via phone</strong> <br> <input id="choice" type="radio" name="communication" value="mail"><strong>via email</strong>';
-    tableEdit.rows[rowNumber].cells[7].innerHTML = '<select id="englishLevel" class="selectLanguage" name="englishLevel" required> <option value="">Please choose</option> <option value="A1">A1</option> <option value="A2">A2</option> <option value="B1">B1</option> <option value="B2">B2</option> <option value="C1">C1</option> <option value="C2">C2</option>';
-    tableEdit.rows[rowNumber].cells[8].innerHTML = '<input id="beginDate" type="date" name="startDate" required>';
-    tableEdit.rows[rowNumber].cells[9].innerHTML = '<input id="skillsContent" class="textSkills" type="text" name="skillsCourses" minlength="10" maxlength="50" required>';
-    tableEdit.rows[rowNumber].cells[10].innerHTML = '<input id="motivationLet" class="textMotivate" type="text" name="joinReason" minlength="10" maxlength="50" required>';
-    tableEdit.rows[rowNumber].cells[11].innerHTML = '<input id="studyHome" class="homeStudyBox" type="checkbox" name="confirmStudyFromHome">';
-    tableEdit.rows[rowNumber].cells[12].innerHTML = '<button id="btnEdit" onclick="editSubmission() " class="sbtButton" style="display:none;">Edit</button> <button id="btnSave" onclick="saveEditedSubmission(' + rowNumber + ')" id="btnSave" class="sbtButton" style="display:inline;">Save</button> <button id="btnCancel" onclick="cancelAction(' + rowNumber + ')" id="btnCancel" class="sbtButton" style="display:inline;" margin-top:2em;">Cancel</button> '
-    tableEdit.rows[rowNumber].cells[13].innerHTML = '<button id="btnDelete" onclick="deleteSubmission(' + data.length + ') " class="sbtButton" style="display:none;">Delete</button>';
+    tableEdit.rows[rowNumber].cells[1].innerHTML = '<input id="firstName' + rowNumber + '" type="text" name="firstName" placeholder="Enter your name here:" required>';
+    tableEdit.rows[rowNumber].cells[2].innerHTML = '<input id="surname' + rowNumber + '" type="text" name="surname" placeholder="Enter your surname here:" required>';
+    tableEdit.rows[rowNumber].cells[3].innerHTML = '<input id="email' + rowNumber + '" name="email" type="text" placeholder="Enter your email address here:" required>';
+    tableEdit.rows[rowNumber].cells[4].innerHTML = '<input id="age' + rowNumber + '" type="number" name="age" min="18" max="99" placeholder="Your age:" required>';
+    tableEdit.rows[rowNumber].cells[5].innerHTML = '<input id="gsm' + rowNumber + '" type="text" name="phoneNumber" placeholder="Enter your phone number here:" required>';
+    tableEdit.rows[rowNumber].cells[6].innerHTML = '<input id="choice' + rowNumber + '" type="radio" name="communication" value="phone"><strong>via phone</strong> <br> <input id="choice" type="radio" name="communication" value="mail"><strong>via email</strong>';
+    tableEdit.rows[rowNumber].cells[7].innerHTML = '<select id="englishLevel' + rowNumber + '" class="selectLanguage" name="englishLevel" required> <option value="">Please choose</option> <option value="A1">A1</option> <option value="A2">A2</option> <option value="B1">B1</option> <option value="B2">B2</option> <option value="C1">C1</option> <option value="C2">C2</option>';
+    tableEdit.rows[rowNumber].cells[8].innerHTML = '<input id="beginDate' + rowNumber + '" type="date" name="startDate" required>';
+    tableEdit.rows[rowNumber].cells[9].innerHTML = '<input id="skillsContent' + rowNumber + '" class="textSkills" type="text" name="skillsCourses" minlength="10" maxlength="50" required>';
+    tableEdit.rows[rowNumber].cells[10].innerHTML = '<input id="motivationLet' + rowNumber + '" class="textMotivate" type="text" name="joinReason" minlength="10" maxlength="50" required>';
+    tableEdit.rows[rowNumber].cells[11].innerHTML = '<input id="studyHome' + rowNumber + '" class="homeStudyBox" type="checkbox" name="confirmStudyFromHome">';
+    tableEdit.rows[rowNumber].cells[12].innerHTML = '<button id="btnEdit' + rowNumber + '" onclick="editSubmission() " class="sbtButton" style="display:none;">Edit</button> <button id="btnSave" onclick="saveEditedSubmission(' + rowNumber + ')" id="btnSave" class="sbtButton" style="display:inline;">Save</button> <button id="btnCancel" onclick="cancelAction(' + rowNumber + ')" id="btnCancel" class="sbtButton" style="display:inline;" margin-top:2em;">Cancel</button> '
+    tableEdit.rows[rowNumber].cells[13].innerHTML = '<button id="btnDelete' + rowNumber + '" onclick="deleteSubmission(' + data.length + ') " class="sbtButton" style="display:none;">Delete</button>';
 }
 
 function cancelAction(rowNumber) {
@@ -119,18 +119,18 @@ function cancelAction(rowNumber) {
 }
 
 function saveEditedSubmission (rowNumber){
-    var firstNameVal = document.getElementById('firstName').value; 
-    var surnameVal = document.getElementById('surname').value;
-    var emailVal = document.getElementById('email').value;
-    var ageVal = document.getElementById('age').value;
-    var gsmVal = document.getElementById('gsm').value;
-    var choiceVal = document.getElementById('choice').value;
-    var englishVal = document.getElementById('englishLevel').value;
-    var dateVal = document.getElementById('beginDate').value;
-    var skillsVal = document.getElementById('skillsContent').value;
-    var motivationVal = document.getElementById('motivationLet').value;
+    var firstNameVal = document.getElementById('firstName' + rowNumber).value; 
+    var surnameVal = document.getElementById('surname' + rowNumber).value;
+    var emailVal = document.getElementById('email' + rowNumber).value;
+    var ageVal = document.getElementById('age' + rowNumber).value;
+    var gsmVal = document.getElementById('gsm' + rowNumber).value;
+    var choiceVal = document.getElementById('choice' + rowNumber).value;
+    var englishVal = document.getElementById('englishLevel' + rowNumber).value;
+    var dateVal = document.getElementById('beginDate' + rowNumber).value;
+    var skillsVal = document.getElementById('skillsContent' + rowNumber).value;
+    var motivationVal = document.getElementById('motivationLet' + rowNumber).value;
     var studyFromHome = '';
-    if(document.getElementById('studyHome').checked){
+    if(document.getElementById('studyHome' + rowNumber).checked){
         studyFromHome = 'Yes'
     } else {
         studyFromHome = 'No'
@@ -151,12 +151,13 @@ function saveEditedSubmission (rowNumber){
         homeStudy: studyFromHome 
         }
         //splice existing object from array
-        data.splice(rowNumber, 1)
         //splice udpated object in previous object place
-        data.splice(rowNumber, 1, newSubmission);
+        data.splice(rowNumber -1, 1, newSubmission);
         //add submission to browse section
+
     }
 }; 
+
 
 
 // function deleteSubmission() {
